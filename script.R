f65f68c3172b6a104db82e1a9b5f081b1dde82e1
nwolko <nico.wolko@gmail.com>
2025-10-31 09:24:13 -0300

    orden de scripts

--- script.R
+++ script.R
@@ -1,40 +1,40 @@
+## Paquetes
+
 library(tidyverse)
 library(sf)
 install.packages("plotly")
 library(plotly)
 
-
+## Algunos datasets no tan trabajados
 evolucion_co2 <- read.csv("data/Argendata//CAMCLI/01_evolucion_CO2_historico.csv")
 nivel_mar <- read.csv("data/CAMCLI/06_evolucion_nivel_del_mar_1993_2022.csv")
 diferencia_paises <- read.csv("data/CAMCLI/diferencia_temperatura_paises.csv")
-
-
 emisiones_mundo <- read_csv("data/Argendata/CAMCLI/emisiones_arg_mundo.csv")
 emisiones_global <- read_csv("data/Argendata/CAMCLI/emisiones_global_sec_1850_2014.csv")
 emisiones_global2 <- read_csv("data/Argendata/CAMCLI/emisiones_sector_global_2016.csv")
 emisiones_mundo <- read_csv("data/Argendata/CAMCLI/emisiones")
-
-
 intensidad <- read_csv("data/Argendata/TRANEN/intensidad_carbono_electri_mundo.csv")
 energ <- read_csv("data/Argendata/TRANEN/intensidad_energ_mundo.csv")
 
-rm(list = ls())
 
+## Empezamos a trabajar con esta
 prueba <- read.csv("data/datos_gob_ar/emisiones_datos_totales_1990_2022.csv", sep = ";")
 
-
+## Primeras pruebas para pasar datos a tablas
 prueba %>% group_by(año, tipo_de_gas) %>%  summarise(tipo_de_gas,
                                         Emisiones = sum(valor_en_toneladas_de_co2e))
 
-
-prueba1 <- prueba %>% 
+## Agrupaciones por emisión y tipo de gas
+tabla_tipo_gas <- prueba %>% 
   group_by(año, tipo_de_gas) %>%  
   summarise(Emisiones = sum(valor_en_toneladas_de_co2e))
 
-prueba2 <- prueba %>% 
+tabla_tipo_gas_ok <- prueba %>% 
   group_by(año, actividad) %>% 
   summarise(Emisiones = sum(valor_en_toneladas_de_co2e))
 
+
+## Primeros gráficos para trabajar los datos
 plot_evolucion_gases <- 
   ggplot(data = prueba1, aes(x= año, y = Emisiones, color = tipo_de_gas)) +
   geom_line() +
@@ -55,6 +55,7 @@ ggplot(data = prueba2, aes(x = Emisiones, color = actividad)) +
   geom_density() +
   theme_minimal()
 
+## Datos
 prueba3 <- prueba2 %>%
   group_by(año) %>%
   mutate(Proporcion = Emisiones / sum(Emisiones) * 100) %>%
@@ -106,7 +107,5 @@ grafico <- ggplot(prueba3, aes(x = año, y = Proporcion, fill = actividad,
   )
 
 ggplotly(grafico, tooltip = "text")
-
-
 
 
